58b6cdc6d6b4e9ba5efb724a1722bb367976ba51
Mitch Hammond <mitch.b.hammond@gmail.com>
2023-11-07 11:10:44 -0700

    minor changes to theme

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,1 +1,10 @@
 // theme object here
+export default {
+    primaryColor: "royalblue",
+    secondaryColor: "crimson",
+    tertiaryColor: "purple",
+    black: 'black',
+    white: 'White',
+    breakpointMobile: '(max-width: 550px)',
+    
+}
